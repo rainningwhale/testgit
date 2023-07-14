c07a3c2eddc90d371587c8c9bd6473480445fa54
rainwhale <1173137842@qq.com>
2023-07-14 16:19:37 +0800

    this is my gittest  Please enter the commit message for our changes. Lines starting

---
+++ testvueandgit/src/components/bottom.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="bottom" >boxbottom</div>
+    <button @click="btnclick" class="btn">btn</button>
+    <div>{{ bottomdata.text}}</div>
+    <p :class="{ active: isActive }" >Hi!</p>
+</template>
+
+
+<script > 
+   export default{
+     name: 'bottom',
+     data() {
+      return {
+        isActive: true,
+        bottomdata:{
+            text: 1
+        }
+      }   
+     },
+     methods: {
+      btnclick(func){
+        this.bottomdata.text++;
+        this.isActive = !this.isActive;
+        if(func){
+          console.log(func);
+        }
+       
+      }
+     }
+
+  
+
+
+   }
+</script>
+
+<style scoped>
+  .bottom{
+    height: 100px;
+    width: 100px;
+    background-color: aqua;
+  }
+
+  .btn{
+    height: 30px;
+    width: 30px;
+    
+  }
+
+  .active{
+    font-size: 20px;
+    color: red;
+  }
+</style>
---
+++ testvueandgit/src/components/top.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="top" id="topbox">box{{ topdata.username }}</div>
+</template>
+
+
+<script>
+export default {
+    name: 'top',
+    data() {
+      return {
+        topdata:{
+            username: 'huzhihong',
+            password: 123
+        }
+    }
+    }
+      
+}
+</script>
+
+<style scoped>
+  #topbox{
+    color: azure;
+    height: 10vw;
+    widows: 10vw;
+    background-color:black;
+  }
+</style>
